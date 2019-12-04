8f4a79d1b52fc71d77043f80a52973695e75c8f0
AjsonZ <51015437+AjsonZ@users.noreply.github.com>
2019-12-04 03:48:14 -0500

    create map and monster model

--- Scripts/Character.gd
+++ Scripts/Character.gd
@@ -3,23 +3,29 @@ extends KinematicBody
 onready var camera = $Pivot/Camera
 
 var gravity = -30
+var jump_speed = 12
 var max_speed = 8
 var mouse_sensitivity = 0.002
 
 var velocity = Vector3()
+var jump = false
 
 func get_input():
-    var input_dir = Vector3()
-    if Input.is_action_pressed("move_forward"):
+	jump = false
+	if Input.is_action_just_pressed("jump"):
+		jump = true
+	var input_dir = Vector3()
+	if Input.is_action_pressed("move_forward"):
         input_dir += -camera.global_transform.basis.z
-    if Input.is_action_pressed("move_back"):
+	if Input.is_action_pressed("move_back"):
         input_dir += camera.global_transform.basis.z
-    if Input.is_action_pressed("strafe_left"):
+	if Input.is_action_pressed("strafe_left"):
         input_dir += -camera.global_transform.basis.x
-    if Input.is_action_pressed("strafe_right"):
+	if Input.is_action_pressed("strafe_right"):
         input_dir += camera.global_transform.basis.x
-    input_dir = input_dir.normalized()
-    return input_dir
+
+	input_dir = input_dir.normalized()
+	return input_dir
 	
 
 func _unhandled_input(event):
@@ -30,9 +36,11 @@ func _unhandled_input(event):
         
 
 func _physics_process(delta):
-    velocity.y += gravity * delta
-    var desired_velocity = get_input() * max_speed
+	velocity.y += gravity * delta
+	var desired_velocity = get_input() * max_speed
 
-    velocity.x = desired_velocity.x
-    velocity.z = desired_velocity.z
-    velocity = move_and_slide(velocity, Vector3.UP, true)
+	velocity.x = desired_velocity.x
+	velocity.z = desired_velocity.z
+	velocity = move_and_slide(velocity, Vector3.UP, true)
+	if jump and is_on_floor():
+		velocity.y = jump_speed
